4bbbe913be50ed22fd3fe4dba3bc14fb770863b6
Evan <evantsaip@gmail.com>
2015-06-20 18:52:31 +0800

    first commit

---
+++ Day0620CollectionViewHW/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  Day0620CollectionViewHW
+//
+//  Created by lalaleelala on 6/20/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
---
+++ Day0620CollectionViewHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day0620CollectionViewHW
+//
+//  Created by lalaleelala on 6/20/15.
+//  Copyright (c) 2015 lalaleelala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
